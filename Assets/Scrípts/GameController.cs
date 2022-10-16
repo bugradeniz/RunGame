6c05b90fc29758115f2b98a6e49a40b188009e22
bugradeniz <bugra098@gmail.com>
2022-10-16 15:53:46 +0300

    pooling sistemi kuruldu.

--- Assets/Scrípts/GameController.cs
+++ Assets/Scrípts/GameController.cs
@@ -8,19 +8,32 @@ public class GameController : MonoBehaviour
     public GameObject clonePrefab;
     public GameObject startPoint;
     public GameObject endPoint;
+    public List<GameObject> clonePool;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         // tusa basildiginda klonlarin olusturulmasi saglandi.
         // ileride degistirilecek
-        if (Input.GetKey(KeyCode.Mouse1)) {
+        // klone havuzundaki aktif olmayan ilk klonu konumlandirip aktif hale getirilmesi saglandi instantiate komutu performans icin kullanilmayacak
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            foreach (GameObject clone in clonePool)
+            {
+                if (!clone.activeInHierarchy)
+                {
+                    
+                    clone.transform.position = startPoint.transform.position;
+                    clone.SetActive(true);
+                    break;
+                }
+            }
 
-            Instantiate(clonePrefab, startPoint.transform.position, Quaternion.identity);
-        
+            // Instantiate(clonePrefab, startPoint.transform.position, Quaternion.identity);
+
         }
     }
 }
